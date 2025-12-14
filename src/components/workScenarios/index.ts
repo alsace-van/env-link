ae6cfcbdc10fa23884b449a79e55076180f36191
alsace-van <alsacevancreation@hotmail.com>
2025-12-14 17:24:45 +0100

    Create index.ts

---
+++ src/components/workScenarios/index.ts
@@ -0,0 +1,7 @@
+// components/workScenarios/index.ts
+// Exports des composants de gestion des scénarios de travaux
+
+export { default as WorkScenarioManager } from './WorkScenarioManager';
+export { default as WorkScenarioColumn } from './WorkScenarioColumn';
+export { default as WorkTaskCard } from './WorkTaskCard';
+export { default as AddWorkTaskDialog } from './AddWorkTaskDialog';
